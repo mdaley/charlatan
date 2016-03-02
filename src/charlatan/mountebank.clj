c2147c5358163fcc67c7859704339e798cfa6569
Matthew Daley <matt.daley@blueyonder.co.uk>
2016-03-02 16:21:01 +0000

    A macro called 'with-mb' that allows a body to be wraped with dynamically bound mountebank instance; nice for testing.

--- src/charlatan/mountebank.clj
+++ src/charlatan/mountebank.clj
@@ -28,10 +28,13 @@
               :mock ["--mock"]
               :debug ["--debug"]})
 
+(def ^:dynamic *mb* nil)
+
 (defn- kvp->args
   "Convert a key value pair of the options to the right form for mountebank -
    either a valid parameter and a value or a valid parameter by itself.
-   An exception is thrown if an invalid key is supplied."
+   An exception is thrown if an invalid key is supplied. Conch key/values
+   are not changed."
   [[k v]]
   (if (contains? conch-keys k)
     [k v]
@@ -41,33 +44,62 @@
         [(first mb-valdef)])
       (throw (Exception. (str "Invalid argument '" k "'."))))))
 
+(defn- conch-keys-last
+  "It seems that the conch keys should go last."
+  [options]
+  (into (sorted-map-by (fn [k1 _] (if (contains? conch-keys k1) 1 -1))) options))
+
 (defn- options->args
   "Convert options map to args required by mountebank."
   [options]
-  (flatten (mapv kvp->args options)))
+  (flatten (mapv kvp->args (conch-keys-last options))))
+
+(defn process-not-running?
+  [p]
+  (try
+    (.exitValue p)
+    true
+    (catch IllegalThreadStateException _ false)))
 
 (defn start
-  "Starts a mountebank server"
+  "Starts a mountebank server. If options are empty or not supplied mb is launched on the default port 2525."
   [& [options]]
-  (let [args (options->args options)
+  (let [launch-delay (or (:delay options) 500)
+        args (options->args (dissoc options :delay))
         p (proc "mb" args)]
     (future (stream-to-out p :out))
     (future (stream-to-out p :err))
-    p))
+    (Thread/sleep launch-delay) ;; A small delay so process gets going.
+    (when (process-not-running? (:process p))
+      (throw (Exception. "Could not start mountebank. Is a process already using the chosen port?")))
+    (assoc p :options (merge {:port 2525} options))))
 
 (defn stop
   "Stops the server"
   [p]
   (destroy p))
 
+(defmacro with-mb
+  "Launches a mountebank instance and evaluates body in its context; at the end stops the mountebank process."
+  [mb-opts & body]
+  `(binding [*mb* (start ~mb-opts)] (try ~@body (finally (stop *mb*)))))
+
 (defn mburl+
+  "Build url for a mountebank request."
   [mb-port & [suffix]]
   (apply str "http://localhost:" mb-port suffix))
 
+(defn- mb-port
+  "Obtain the port of the dynamically bound *mb*"
+  []
+  (-> *mb* :options :port))
+
 (defn get-config
-  [mb-port]
-  (http/get (mburl+ mb-port "/config")
-            {:as :json}))
+  ([]
+   (get-config (mb-port)))
+  ([mb-port]
+   (http/get (mburl+ mb-port "/config")
+             {:as :json})))
 
 (defn- create-imposter-data
   [{:keys [port protocol name stubs key cert mode
@@ -85,18 +117,24 @@
 (defn create-imposter
   "Create an imposter. Note, if you want an imposter on a random port,
    pass in port = nil."
-  [mb-port port & [params]]
-  (let [body (json/generate-string (create-imposter-data (assoc params :port port)))]
-    (println "BODY" body)
-    (http/post (mburl+ mb-port "/imposters")
-               {:as :json
-                :content-type "application/json"
-                :body body})))
+  ([port]
+   (create-imposter port nil))
+  ([port params]
+   (create-imposter (mb-port) port params))
+  ([mb-port port params]
+   (let [body (json/generate-string (create-imposter-data (assoc params :port port)))]
+     (println "BODY" body)
+     (http/post (mburl+ mb-port "/imposters")
+                {:as :json
+                 :content-type "application/json"
+                 :body body}))))
 
 (defn get-imposter
-  [mb-port port]
-  (http/get (mburl+ mb-port "/imposters/" port)
-            {:as :json}))
+  ([port]
+   (get-imposter (mb-port) port))
+  ([mb-port port]
+   (http/get (mburl+ mb-port "/imposters/" port)
+             {:as :json})))
 
 (defn- replay-and-proxy-params
   [{:keys [replayable remove-proxies]}]
@@ -105,28 +143,42 @@
     (not (nil? remove-proxies)) (assoc :removeProxies remove-proxies)))
 
 (defn delete-imposter
-  [mb-port port & [params]]
-  (http/delete (mburl+ mb-port "/imposters/" port)
-               {:as :json
-                :query-params (replay-and-proxy-params params)}))
+  ([port]
+   (delete-imposter port nil))
+  ([port params]
+   (delete-imposter (mb-port) port params))
+  ([mb-port port params]
+   (http/delete (mburl+ mb-port "/imposters/" port)
+                {:as :json
+                 :query-params (replay-and-proxy-params params)})))
 
 (defn list-imposters
-  [mb-port & [params]]
-  (http/get (mburl+ mb-port "/imposters")
-            {:as :json
-             :query-params (replay-and-proxy-params params)}))
+  ([]
+   (list-imposters nil))
+  ([params]
+   (list-imposters (mb-port) params))
+  ([mb-port params]
+   (http/get (mburl+ mb-port "/imposters")
+             {:as :json
+              :query-params (replay-and-proxy-params params)})))
 
 (defn replace-imposters
   "Replace all existing imposters with a new set. `imposters` is a list of
    one or more new imposters."
-  [mb-port imposters]
-  (http/put (mburl+ mb-port "/imposters")
-            {:as :json
-             :content-type "application/json"
-             :body (json/generate-string {:imposters (map create-imposter-data imposters)})}))
+  ([imposters]
+   (replace-imposters (mb-port) imposters))
+  ([mb-port imposters]
+   (http/put (mburl+ mb-port "/imposters")
+             {:as :json
+              :content-type "application/json"
+              :body (json/generate-string {:imposters (map create-imposter-data imposters)})})))
 
 (defn delete-imposters
-  [mb-port & [params]]
-  (http/delete (mburl+ mb-port "/imposters")
-               {:as :json
-                :query-params (replay-and-proxy-params params)}))
+  ([]
+   (delete-imposters nil))
+  ([params]
+   (delete-imposters (mb-port) params))
+  ([mb-port params]
+   (http/delete (mburl+ mb-port "/imposters")
+                {:as :json
+                 :query-params (replay-and-proxy-params params)})))
--- test/charlatan/core_test.clj
+++ test/charlatan/core_test.clj
@@ -8,29 +8,32 @@
 (def mb-port 2525)
 (def port 8080)
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
-
 (deftest simple-response-to-ping
-  (testing "mountebank launched, set with stub, request made, correct response, stop mountebank"
+  (testing "mountebank simple response stub"
     (let [m (mb/start {:port mb-port})]
       (try
-        (println "M" m)
-        (Thread/sleep 1000)
         (mb/create-imposter mb-port port
                             {:stubs [{:responses [{:is {:statusCode 202
                                                         :body "pong"}}]
                                       :predicates [{:equals {:method "GET"
                                                              :path "/ping"}}]}]})
-        (println "CREATED IMPOSTER")
-        (Thread/sleep 1000)
         (let [response (http/get (str "http://localhost:" port "/ping")
                                  {:throw-exceptions false})]
-          (println "RESPONSE" response)
           (is (= (:status response) 202))
           (is (= (:body response) "pong"))
           )
         (finally
           (println "FINALLY")
           (mb/stop m))))))
+
+(deftest test-with-approach
+  (testing "mountebank simple response stub inside with-mb macro"
+    (mb/with-mb {:port 2567}
+      (mb/create-imposter port  {:stubs [{:responses [{:is {:statusCode 202
+                                                            :body "pong"}}]
+                                          :predicates [{:equals {:method "GET"
+                                                                 :path "/ping"}}]}]})
+      (let [{status :status body :body} (http/get (str "http://localhost:" port "/ping")
+                                                  {:throw-exceptions false})]
+        (is (= status 202))
+        (is (= body "pong"))))))
