d9badfc7cb3a81a9856c7b8bcc51e691119383ba
Matthew Daley <matt.daley@blueyonder.co.uk>
2016-03-02 16:32:15 +0000

    Depend on clojure 1.6 so more widely useful.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[cheshire "5.5.0"]
                  [clj-http "2.0.1"]
                  [me.raynes/conch "0.8.0"]
-                 [org.clojure/clojure "1.8.0"]]
+                 [org.clojure/clojure "1.6.0"]]
   :scm {:name "git"
         :url "https://github.com/mdaley/charlatan"}
   :repositories [["releases" {:url "https://clojars.org/repo"
